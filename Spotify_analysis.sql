9a479b50bbcb0dd214af2e85171f8530daf1ff2c
Nitin Chaudhary <57886482+nitin35byte@users.noreply.github.com>
2023-09-22 23:19:32 +0530

    Update Spotify_analysis.sql

--- Spotify_analysis.sql
+++ Spotify_analysis.sql
@@ -2,11 +2,11 @@
 
 SELECT artist_name, COUNT(*) AS Number_of_Songs FROM Spotify GROUP BY artist_name ORDER BY COUNT(*) DESC;
 
-# ---- Top 10 songs based on popularity ------
+# -------------------- Top 10 songs based on popularity ---------------------------------
 select * from spotify;
 select track_name ,track_popularity , album ,album from spotify order by track_popularity desc limit 10;
 
-#---- Total number of songs on spotify based on year -----
+#-------------------------- Total number of songs on spotify based on year -----------------------------------
 
 select `year` track_name , count(track_name) as song_count from spotify group by `year` order by `year` desc;
 
@@ -15,7 +15,7 @@ select `year` track_name , count(track_name) as song_count from spotify group by
 select `year` , track_id,track_name,track_popularity as popularity from (
 select `year` , track_id,track_name,track_popularity, row_number() over(partition by `year` order by track_popularity desc ) as `rank` from spotify) as song_rank
 where `rank` =1;
-# ------------------Another way to achive that with cte
+# ------------------Another way to achive that with cte ----------------------------------
 with RankedSongsas as (select year , track_name , track_popularity,
 row_number() over(partition by year order by track_popularity desc) as `rank`
 from spotify where year between 2000 and 2022
@@ -25,7 +25,7 @@ select year,
     track_name AS top_song,
     track_popularity from RankedSongsas where `rank` =1 order by track_popularity desc;
 
-#---------------------------- Analysis based on Tempo ---
+#---------------------------- Analysis based on Tempo ----------------------------
 
 select round(avg(tempo)) from spotify;
 
@@ -40,7 +40,7 @@ select track_name,tempo ,
 
  select track_name , tempo from spotify  order by tempo desc limit 10 offset 0;
  
- #---- Number of Songs for different Tempo Range ----
+ #---------------------- Number of Songs for different Tempo Range ------------------------------
  
  select distinct tempo , min(tempo) , max(tempo) from spotify order by tempo desc;
 
@@ -53,6 +53,7 @@ FROM spotify;
 
 
 #----------------------------- Energy Analysis ----------------
+
 SELECT ENERGY FROM spotify where energy>1;
 SELECT ROUND(avg(ENERGY)) FROM spotify order by energy DESC;
  SELECT
